feff42c58b3e3130b62dc1120f4ee54060a70a41
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2022-12-25 22:08:40 -0300

    Update no UI, tentativa 3

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
-// Consts
-const inputsPlayer = document.querySelectorAll('header [data-param]');
-const inputsEnemy = document.querySelectorAll('footer [data-param]');
+/* Easy way to disable the entire JavaScript file, for HTML/CSS changes */
 
-//
+//throw Error("JavaScript disabled temporarily for debugging purposes.");
+
+/* Consts */
+// Main Screen
+const inputsPlayer = document.querySelectorAll('header [data-attribute], .main__attributes:first-child [data-attribute]');
+const inputsEnemy = document.querySelectorAll('footer [data-attribute], .main__attributes:last-child [data-attribute]');
+// Levelup Screen
 const levelupScreen = document.querySelector('[data-levelup="screen"]');
-levelupScreen.querySelectorAll("button").forEach((val, ind, arr) => {
+const levelupInputs = document.querySelectorAll('[data-levelup="input"]');
+const levelupBtnDecrease = document.querySelectorAll('[data-levelup="btn-minus"]');
+const levelupBtnIncrease = document.querySelectorAll('[data-levelup="btn-plus"]');
+const levelupBtnConfirm = document.querySelector('[data-levelup="confirm"');
+
+/* List:
+    0 = Combat;
+    1 = Level-Up;
+*/
+var currentScreen = 0;
+
+function levelupModifyAttribute(attribute, action) {
+    // Defines if attribute should be raised or lowered
+    let values = [];
+    if(!action) {
+        values = [5, 1];
+    } else {
+        values = [-5, -1];
+    }
+    // Statements
+    switch(true) {
+        case attribute === 0:
+            playerActor.maxHealth += values[0];
+            break;
+        case attribute === 1:
+            playerActor.attack += values[1];
+            break;
+        case attribute === 2:
+            playerActor.defence += values[1];
+            break;
+        case attribute === 3:
+            playerActor.agility += values[1];
+            break;
+        case attribute === 4:
+            playerActor.favor += values[1];
+            break;
+        default:
+            throw Error("Verify function levelupChanges()");
+    }
+}
+
+/* Level-up query selectors */
+// Decrease assigned points
+levelupBtnDecrease.forEach((val, ind, arr) => {
     val.addEventListener("click", event => {
-        switch(true) {
-            case ind === 0:
-                playerActor.maxHealth += 5;
-                break;
-            case ind === 1:
-                playerActor.attack += 1;
-                break;
-            case ind === 2:
-                playerActor.defence += 1;
-                break;
-            case ind === 3:
-                playerActor.agility += 1;
-                break;
-            case ind === 4:
-                playerActor.favor += 1;
-                break;
-            default:
-                throw Error("Invalid index, check code for oversights");
+        if(playerActor.curSpread[ind] > 0) {
+            playerActor.curPoints++;
+            playerActor.curSpread[ind] -= 1;
+            levelupModifyAttribute(ind, 1);
+            updateScreen();
         }
-        playerActor.curHealth = playerActor.maxHealth;
-        levelView(0);
+    });
+})
+// Assign points to attributes
+levelupBtnIncrease.forEach((val, ind, arr) => {
+    val.addEventListener("click", event => {
+        if(playerActor.curPoints > 0) {
+            playerActor.curPoints--;
+            playerActor.curSpread[ind] += 1;
+            levelupModifyAttribute(ind, 0);
+            updateScreen();
+        }
     });
 });
+// Confirm selection
+levelupBtnConfirm.addEventListener("click", event => {
+    if(playerActor.curPoints === 0) {
+        playerActor.curHealth = playerActor.maxHealth;
+        levelView(0);
+    }
+});
+
 // Runs or pauses the combat
 let pause = false;
 
@@ -37,12 +88,17 @@ class Actor {
     constructor(level, health, attack, defence, agility, favor) {
         this.level = level;
         this.curExp = 0;
+        this.maxPoints = Math.floor(Math.cbrt(this.level));
+        this.curPoints = this.maxPoints;
+        this.curSpread = [0, 0, 0, 0, 0];
         this.maxHealth = health;
         this.curHealth = health;
         this.attack = attack;
         this.defence = defence;
         this.agility = agility;
+        this.curHits = 0;
         this.favor = favor;
+        this.curFavor = 0;
     }
 }
 // Shows or hides the levelup screen
@@ -50,21 +106,30 @@ function levelView(bool) {
     if(bool) {
         pause = true;
         levelupScreen.classList.add("js-flex");
+        currentScreen = 1;
     } else if (!bool) {
         pause = false;
         levelupScreen.classList.remove("js-flex");
+        currentScreen = 0;
     }
 }
 
 // Checks if level-up is possible
 function levelCheck() {
     playerActor.curExp++;
-    if(playerActor.curExp >= Math.floor(Math.sqrt(playerActor.level))) {
+    if(playerActor.curExp >= Math.floor(Math.cbrt(playerActor.level))) {
         playerActor.curExp = 0;
+        playerActor.maxPoints = 2;
+        // Original formula: Math.floor(Math.cbrt(playerActor.level))
+        playerActor.curPoints = playerActor.maxPoints;
+        playerActor.curSpread.forEach((val, ind, arr) => {
+            arr[ind] = 0;
+        });
         playerActor.level++;
         levelView(1);
     }
     enemyActor = generateEnemy();
+    getTurnOrder();
 }
 // Returns a random number, minimum of "min" and maximum of "max - 1"
 function randomNumber(min, max) {
@@ -116,19 +181,42 @@ let enemyActor = generateEnemy();
 
 /* Combat related */
 
-// Turn order
-let turnOrder;
-if(playerActor.agility > enemyActor.agility) {
-    turnOrder = 0;
-} else if(enemyActor.agility > playerActor.agility) {
-    turnOrder = 1;
-} else if(randomNumber(0, 2)) {
-    turnOrder = 0;
-} else {
-    turnOrder = 1;
+// Get actor curHits
+function getCurrentHits(actor) {
+    if(!actor) {
+        playerActor.curHits = Math.floor(playerActor.agility / enemyActor.agility);
+        if(playerActor.curHits <= 0) {
+            playerActor.curHits = 1;
+        }
+    } else {
+        enemyActor.curHits = Math.floor(enemyActor.agility / playerActor.agility);
+        if(enemyActor.curHits <= 0) {
+            enemyActor.curHits = 1;
+        }
+    }
 }
+
+// Define turn order and curHits
+let turnOrder;
+function getTurnOrder() {
+    if(playerActor.agility > enemyActor.agility) {
+        turnOrder = 0;
+    } else if(enemyActor.agility > playerActor.agility) {
+        turnOrder = 1;
+    } else {
+        let randomOrder = randomNumber(0, 2);
+        if(randomOrder) {
+            turnOrder = 0;
+        } else {
+            turnOrder = 1;
+        }
+    }
+    getCurrentHits(turnOrder);
+}
+getTurnOrder();
+
 // Damage
-function causeDamage(turn, isCrit) {
+function causeDamage(isCrit) {
     let baseDamage;
     if(turnOrder === 0) {
         baseDamage = playerActor.attack - enemyActor.defence;
@@ -162,50 +250,94 @@ function combatResult() {
 // Combat Function
 function combat() {
     if(turnOrder === 0) {
-        if(randomNumber(1, 100) <= playerActor.favor) {
-            console.log("Player Critical Hit!");
-            enemyActor.curHealth -= causeDamage(turnOrder, 1);
+        playerActor.curFavor += playerActor.favor;
+        // Check if Player critical hit is possible
+        if(playerActor.curFavor >= 100) {
+            console.log("PC Crit");
+            playerActor.curFavor = 0;
+            enemyActor.curHealth -= causeDamage(1);
         } else {
-            enemyActor.curHealth -= causeDamage(turnOrder, 0);;
+            enemyActor.curHealth -= causeDamage(0);
         }
-        turnOrder = 1;
+        // Player attacks "curHits" times
+        playerActor.curHits--;
+        if(playerActor.curHits <= 0) {
+            turnOrder = 1;
+            getCurrentHits(turnOrder);
+        }
     } else {
-        if(randomNumber(1, 100) <= enemyActor.favor) {
+        enemyActor.curFavor += enemyActor.favor;
+        // Check if Enemy critical hit is possible
+        if(enemyActor.curFavor >= 100) {
             console.log("Enemy Critical Hit!");
-            playerActor.curHealth -= causeDamage(turnOrder, 1);
+            enemyActor.curFavor = 0;
+            playerActor.curHealth -= causeDamage(1);
         } else {
-            playerActor.curHealth -= causeDamage(turnOrder, 0);
+            playerActor.curHealth -= causeDamage(0);
         }
-        turnOrder = 0;
+        // Player attacks "curHits" times
+        enemyActor.curHits--;
+        if(enemyActor.curHits <= 0) {
+            turnOrder = 0;
+            getCurrentHits(turnOrder);
+        }
     }
     combatResult();
 }
 // Update player and enemy attributes (e.g.: HP)
 function updateScreen() {
-    // Updates PC
-    inputsPlayer[0].value = playerActor.level;
-    inputsPlayer[1].value = `${playerActor.curHealth}/${playerActor.maxHealth}`;
-    inputsPlayer[2].value = playerActor.attack;
-    inputsPlayer[3].value = playerActor.defence;
-    inputsPlayer[4].value = playerActor.agility;
-    inputsPlayer[5].value = playerActor.favor;
-    // Updates NPC
-    inputsEnemy[0].value = enemyActor.level;
-    inputsEnemy[1].value = `${enemyActor.curHealth}/${enemyActor.maxHealth}`;
-    inputsEnemy[2].value = enemyActor.attack;
-    inputsEnemy[3].value = enemyActor.defence;
-    inputsEnemy[4].value = enemyActor.agility;
-    inputsEnemy[5].value = enemyActor.favor;
+    switch(true) {
+        case currentScreen === 0:
+            // Updates main screen player attributes
+            inputsPlayer[0].value = playerActor.level;
+            inputsPlayer[1].value = `${playerActor.curHealth}/${playerActor.maxHealth}`;
+            inputsPlayer[2].value = `${playerActor.curFavor}/100`;
+            inputsPlayer[3].value = playerActor.attack;
+            inputsPlayer[4].value = playerActor.defence;
+            inputsPlayer[5].value = playerActor.agility;
+            // Updates main screen enemy attributes
+            inputsEnemy[0].value = enemyActor.attack;
+            inputsEnemy[1].value = enemyActor.defence;
+            inputsEnemy[2].value = enemyActor.agility;
+            inputsEnemy[3].value = enemyActor.level;
+            inputsEnemy[4].value = `${enemyActor.curHealth}/${enemyActor.maxHealth}`;
+            inputsEnemy[5].value = `${enemyActor.curFavor}/100`;
+            break;
+        case currentScreen === 1:
+            // Updates levelup screen
+            document.querySelector('[data-attribute="currentPoints"]').innerText = playerActor.curPoints;
+            levelupInputs[0].value = playerActor.maxHealth;
+            levelupInputs[1].value = playerActor.attack;
+            levelupInputs[2].value = playerActor.defence;
+            levelupInputs[3].value = playerActor.agility;
+            levelupInputs[4].value = playerActor.favor;
+            break;
+    }
+    
+    
 }
 // Runs everything periodically
 function executeTurn() {
     // runs only if game isn't paused
     if(!pause) {
-    combat();
-    // updates screen after everything
-    updateScreen();
+        combat();// updates screen after everything
+        updateScreen();
     }
 }
 
+/* Hotkeys */
+// Space
+document.addEventListener("keypress", event => {
+    console.log(event.code);
+    console.log(currentScreen === 1);
+    if(event.code === "Space" && currentScreen === 1) {
+        levelupBtnConfirm.click();
+    }
+})
+
+
 executeTurn();
+/* FPS list
+    60 FPS = 16.67ms
+*/
 setInterval(executeTurn, 1);
